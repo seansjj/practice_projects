920348586d090a9790f83b12154b6ce5881902a2
Sean Jeong <seanj118@gmail.com>
2024-02-20 14:01:24 -0800

    finished game

--- higher_lower_game/higher_lower.py
+++ higher_lower_game/higher_lower.py
@@ -6,7 +6,12 @@ from instagram_data import data
 SLOT_A = random.choice(data)
 SLOT_B = random.choice(data)
 
-SCORE = 18
+# prevents slots A, B being the same
+if SLOT_B == SLOT_A:
+    SLOT_B = random.choice(data)
+
+SCORE = 0
+GAME_OVER = False
 
 def account_blurb(account):
     '''gets name, description, country of instagram account'''
@@ -24,51 +29,35 @@ def account_blurb(account):
 
 def game_answer(account1, account2):
     '''returns a or b for whichever account has more followers'''
-    followers1 = account1.get('follower_count')
-    followers2 = account2.get('follower_count')
-    
-    # tester code for follower count
-    print(f'A: {followers1}. B: {followers2}')
+    followers_a = account1.get('follower_count')
+    followers_b= account2.get('follower_count')
 
-    if followers1 > followers2:
+    if followers_a > followers_b:
         return 'a'
     else:
         return 'b'
 
-def clear(seconds = 0):
-    '''clears text in terminal '''
-    time.sleep(seconds)
-    os.system('clear')
-
-print(logo)
-
-print('Compare A:')
-account_blurb(SLOT_A)
-
-print(vs)
-
-print('Against B:')
-account_blurb(SLOT_B)
-
-guess = input('Who has more followers? Type A or B: ')
-
-if guess.lower() == game_answer(SLOT_A,SLOT_B):
-    print('Correct')
-else:
-    clear()
+while not GAME_OVER:
     print(logo)
-    print('Sorry, you are wrong. Final score: {SCORE}')
+    print(f'Current Score: {SCORE}')
+    print('Compare A:')
+    account_blurb(SLOT_A)
 
+    print(vs)
 
-# find way to clear terminal
+    print('Against B:')
+    account_blurb(SLOT_B)
 
-# if another answer, go to game over screen
+    guess = input('Who has more followers? Type A or B: ')
 
-# terminal clears
-
-# score updates on top, comparison B becomes A now
-
-# add case if runs out of questions
-
-# find way for no repeats
-
+    if guess.lower() == game_answer(SLOT_A,SLOT_B):
+        SCORE += 1
+        SLOT_A = SLOT_B
+        SLOT_B = random.choice(data)
+        if SLOT_B == SLOT_A:
+            SLOT_B = random.choice(data)
+    else:
+        GAME_OVER = True
+  
+print(logo)
+print(f'Sorry, you are wrong. Final score: {SCORE}')
