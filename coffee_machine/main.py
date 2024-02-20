44346d4bd5087cff73a55255b0d3183a05efdd86
Sean Jeong <seanj118@gmail.com>
2024-02-20 14:34:36 -0800

    started coffee machine project

---
+++ coffee_machine/main.py
@@ -0,0 +1,12 @@
+
+
+order = input('What would you like? (espresso/latte/cappuccino): ')
+
+if order == 'report':
+    print(resources)
+elif order == 'espresso':
+    print('espresso')
+elif order == 'latte':
+    print('latte')
+elif order == 'cappuccino':
+    print('cappuccino')
