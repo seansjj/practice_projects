4650a8b9d82557f1b743746fb58bc33690d19fb0
Sean Jeong <seanj118@gmail.com>
2024-03-02 16:51:28 -0800

    ordered turtle in random angles

--- dot_art/dot_art.py
+++ dot_art/dot_art.py
@@ -1,8 +1,20 @@
+import random
 from turtle import Screen, Turtle
 
 cursor = Turtle()
+angle = [0, 90, 180, 270]
+colours = ['blue', 'red', 'green', 'yellow']
 
-cursor.forward(100)
+cursor.width(5)
+
+for turtle in range(20):
+    cursor.pencolor(random.choice(colours))
+    cursor.right(random.choice(angle))
+    cursor.forward(25)
+
+# random walk
+# line thick
+# speed up
 
 screen = Screen()
 screen.exitonclick()
